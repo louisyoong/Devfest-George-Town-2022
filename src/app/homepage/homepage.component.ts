feae1dc7c3f25cc86fd2925bc84d4a413b64aa78
Louis <louis@Louiss-MacM1Book-Pro.local>
2022-08-27 20:48:03 +0800

    Add speaker session

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,20 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import {Observable} from 'rxjs';
-// import {MatDialog} from '@angular/material';
-// import {AngularFirestore} from '@angular/fire/firestore';
-// import {SpeakerDialogComponent} from '../_dialog/speaker-dialog/speaker-dialog.component';
+import { Observable } from 'rxjs';
+import { Speaker } from '../model/speaker.model';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.scss']
+  styleUrls: ['./homepage.component.scss'],
 })
 export class HomepageComponent implements OnInit {
+  speakers: Speaker[] = [
+    new Speaker(
+      'Vin Lim',
+      'Google Developer Expert, Web',
+      '/assets/images/speakers/1.png'
+    ),
+    new Speaker(
+      'Vin Lim',
+      'Google Developer Expert, Web',
+      '/assets/images/speakers/2.png'
+    ),
+    new Speaker(
+      'Vin Lim',
+      'Google Developer Expert, Web',
+      '/assets/images/speakers/3.png'
+    ),
+    new Speaker(
+      'Vin Lim',
+      'Google Developer Expert, Web',
+      '/assets/images/speakers/4.png'
+    ),
+  ];
 
-      constructor() { }
+  constructor() {}
 
- 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
